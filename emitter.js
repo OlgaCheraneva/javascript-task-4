2943d936836ece186f92188bc44ce8eff4b9cf2d
OlgaCheraneva <32261747+OlgaCheraneva@users.noreply.github.com>
2018-11-03 10:16:35 +0500

    Update emitter.js

--- emitter.js
+++ emitter.js
@@ -4,13 +4,47 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-const isStar = true;
+const isStar = false;
 
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
 function getEmitter() {
+    let events = new Map();
+
+    class EventListener {
+        constructor(context, handler, times = Infinity, frequency = 1) {
+            this.context = context;
+            this.handler = handler;
+            this.times = times;
+            this.frequency = frequency;
+        }
+    }
+
+    function addListener(event, listener) {
+        if (!events.has(event)) {
+            events.set(event, []);
+        }
+        events.get(event).push({ listener, count: 0 });
+    }
+
+    function deleteListeners(event, context) {
+        const result = events.get(event).filter(element => element.listener.context !== context);
+        events.set(event, result);
+    }
+
+    function respond(event) {
+        events.get(event).forEach(element => {
+            if (element.count % element.listener.frequency === 0 &&
+                element.listener.times > 0) {
+                element.listener.handler.call(element.listener.context);
+                element.listener.times--;
+            }
+            element.count++;
+        });
+    }
+
     return {
 
         /**
@@ -18,26 +52,47 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object}
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            const listener = new EventListener(context, handler);
+            addListener(event, listener);
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
         off: function (event, context) {
-            console.info(event, context);
+            for (const key of events.keys()) {
+                if (key.startsWith(event + '.') || key === event) {
+                    deleteListeners(key, context);
+                }
+            }
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
         emit: function (event) {
-            console.info(event);
+            let currentEvent = event + '.';
+            do {
+                const index = currentEvent.lastIndexOf('.');
+                currentEvent = currentEvent.substring(0, index);
+                if (events.has(currentEvent)) {
+                    respond(currentEvent);
+                }
+            } while (currentEvent.includes('.'));
+
+            return this;
         },
 
         /**
@@ -47,9 +102,13 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object}
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            const listener = new EventListener(context, handler, times);
+            addListener(event, listener);
+
+            return this;
         },
 
         /**
@@ -59,9 +118,13 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object}
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            const listener = new EventListener(context, handler, frequency);
+            addListener(event, listener);
+
+            return this;
         }
     };
 }
